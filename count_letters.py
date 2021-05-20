eab7d84970351b472fa4ad8daede745343b0f66d
Rodrigue Andela <thaandeh@oregonstate.edu>
2021-05-19 18:38:58 -0700

    count_letters.py

---
+++ count_letters.py
@@ -0,0 +1,19 @@
+# Rodrigue Andela
+# 05/19/2021
+# in this task I write a function that take as a parameter as string and return dictionary
+
+# count_letters takes in a string and produces a letter frequency dictionary
+def count_letters(list):
+     letters = {}
+     #for every letter in list
+     for P in list:
+        if P.upper() >= 'A' and P.upper() <= 'Z':
+            if P.upper() in letters:
+                # for every letter recognized adding one count
+                letters[P.upper()]+= 1
+            else:
+                letters[P.upper()] = 1
+                # returning letters dictionary
+     return letters
+
+print(count_letters("AaBb"))
